52ba00d0f1ba1cb088615ae4bc1d7029cff99d6a
zdj <493904954@qq.com>
2021-07-03 10:22:15 +0800

    v1

---
+++ zdj-com/public/zdj.js
@@ -0,0 +1,21 @@
+function jsonp(url) {
+  return new Promise((resolve, reject) => {
+    const random = 'zdjJSONPCallbackName' + Math.random()
+    window[random] = data => {
+      resolve(data)
+    }
+    const script = document.createElement('script')
+    script.src = `${url}?callback=${random}`
+    script.onload = () => {
+      script.remove()
+    }
+    script.onerror = () => {
+      reject()
+    }
+    document.body.appendChild(script)
+  })
+}
+
+jsonp('http://qq.com:8000/friends.js').then(data => {
+  console.log(data)
+})
